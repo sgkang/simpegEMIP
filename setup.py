94f839a4909acbf43dce5e4abb1973e42d512b4e
seogi_macbook <sgkang09@github.com>
2018-08-09 23:12:18 -0700

    modify setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,20 @@ setup(
         'discretize>=0.2.0',
         'geoana>=0.0.4'
     ],
+    install_requires=[
+        'numpy>=1.7',
+        'scipy>=0.13',
+        'setuptools>=18.0',
+        'cython',
+        'pymatsolver>=0.1.1',
+        'ipython',
+        'matplotlib',
+        'properties>=0.5.2',
+        'vectormath>=0.2.0',
+        'SimPEG',
+        'discretize>=0.2.0',
+        'geoana>=0.0.4'
+    ],
     ext_modules=[
         Extension(
             "getJpol",
